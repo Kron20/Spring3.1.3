b737844b9d30b8c824eed0492c279f57e80e1167
Ilya Noskov <ilyanoskov1998@yandex.ru>
2022-04-23 18:19:20 +0300

    111

---
+++ src/main/java/com/spring/springbootbootstrap/Exceptions/UserWrongData.java
@@ -0,0 +1,15 @@
+package com.spring.springbootbootstrap.Exceptions;
+
+public class UserWrongData {
+    private String info;
+
+    public UserWrongData() {}
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
